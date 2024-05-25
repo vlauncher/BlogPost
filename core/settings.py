34d52afd218afa8f927d7f4029181c96b54b8a42
vlauncher <v2dent@gmail.com>
2024-05-25 13:56:38 +0100

    updates v2

--- core/settings.py
+++ core/settings.py
@@ -43,10 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'whitenoise.runserver_nostatic',
     'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
