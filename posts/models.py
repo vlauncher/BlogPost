72040adbeae0166947ea541e6ee2d8a74aea6e91
vlauncher <v2dent@gmail.com>
2024-05-25 14:07:12 +0100

    updates v4

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    overview = models.TextField(blank=True)
+    overview = models.TextField(blank=True, null=True)
     post_url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- posts/services.py
+++ posts/services.py
@@ -4,11 +4,8 @@ import re
 def fetch_github_raw_text(url):
     cleaned_url = re.sub(r'^https?://github\.com/', 'https://raw.githubusercontent.com/', url)
     cleaned_url = re.sub(r'/blob/', '/', cleaned_url)
-    github_headers = {
-                'Authorization': f'token {os.environ.get("GITHUB_TOKEN")}',
-                'Accept': 'application/vnd.github.v3.raw'
-            }
-    response = requests.get(cleaned_url, headers=github_headers)
+    
+    response = requests.get(cleaned_url)
     if response.status_code == 200:
         return response.text
     return None
